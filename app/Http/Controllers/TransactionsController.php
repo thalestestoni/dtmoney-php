999b4ff0d9fdb31bb62b2f1b6610a2206999d290
Thales F Testoni <thalestestoni@gmail.com>
2022-06-21 20:47:51 -0300

    Inicio do transactions controller resources

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -2,13 +2,102 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
+use App\Models\User;
+use App\Models\TypeTransaction;
 use Illuminate\Http\Request;
 
 class TransactionsController extends Controller
 {
-    public function index() {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $transactions = Transaction::all();
+
         return view('transactions');
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $transactionData = $request->all();
+
+        $userId = 1;
+        $user = User::find($userId);
+
+        $typeTransactionId = 1;
+        $typeTransaction = TypeTransaction::find($typeTransactionId);
+
+        $transaction = new Transaction();
+        $transaction->amount = $transactionData['amount'];
+
+        $transaction->user()->associate($user);
+        $transaction->type()->associate($typeTransaction);
+
+        //TODO
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
